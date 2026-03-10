5eef572f8a216c0b6f68012f270bed7f0133daa1
TejwantKaur <kaurtejwant998@gmail.com>
2026-03-10 10:56:32 +0530

    Phase-1, CRUD Operations

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const Listing = require("./models/listing")
+const methodOverride = require("method-override")
 const path = require("path")
-
-const app = express();
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -18,8 +17,12 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+const app = express();
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 
 app.get("/", (req, res) => {
     res.send("Hi I am root")
@@ -31,14 +34,46 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index", { allListings });
 });
 
-//Show Route
+// New Route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+// Create Route
+app.post("/listings", async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save()
+    res.redirect("/listings")
+});
+
+// Show Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listing/show.ejs", { listing });
+    res.render("listings/show.ejs", { listing });
 });
 
+// Edit Route
+app.get("/listings/:id/edit", async (req,res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+});
 
+// update route
+app.put("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing });
+    res.redirect(`/listings/${id}`)
+});
+
+// delete route
+app.delete("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    let delListing = await Listing.findByIdAndDelete(id);
+    console.log(delListing)
+    res.redirect("/listings")
+});
 
 
 
