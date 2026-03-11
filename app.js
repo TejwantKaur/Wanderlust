7918ca98fac4531f7282e7d83147d554e4f0642f
TejwantKaur <kaurtejwant998@gmail.com>
2026-03-11 12:26:08 +0530

    styled our pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const Listing = require("./models/listing")
 const methodOverride = require("method-override")
 const path = require("path")
+const ejsMate = require('ejs-mate')
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -22,8 +23,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate)
+app.use(express.static(path.join(__dirname,"public")))
 
 
+// root
 app.get("/", (req, res) => {
     res.send("Hi I am root")
 });
@@ -35,12 +39,12 @@ app.get("/listings", async (req, res) => {
 });
 
 // New Route
-app.get("/listings/new", (req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
 // Create Route
-app.post("/listings", async(req,res)=>{
+app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save()
     res.redirect("/listings")
@@ -54,21 +58,21 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 // Edit Route
-app.get("/listings/:id/edit", async (req,res)=>{
+app.get("/listings/:id/edit", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit", { listing });
 });
 
 // update route
-app.put("/listings/:id", async(req,res)=>{
+app.put("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing });
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`)
 });
 
 // delete route
-app.delete("/listings/:id", async(req,res)=>{
+app.delete("/listings/:id", async (req, res) => {
     let { id } = req.params;
     let delListing = await Listing.findByIdAndDelete(id);
     console.log(delListing)
